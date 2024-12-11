18b1ea41e553c1dae5317f033cd043fbfe249f92
Jack Gallagher <339690067@GoTVDSB.ca>
2024-12-11 09:07:30 -0500

    Add project files.

---
+++ Topic 9 - Making A Class Lesson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_9___Making_A_Class_Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
